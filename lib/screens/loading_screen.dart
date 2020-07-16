fe73c01bff864bf6964a9b9987a649d5b59bde04
bhattanurag <60163583+bhattanurg@users.noreply.github.com>
2020-07-17 00:26:36 +0530

    refactoring code to get current longitude and latitude

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -15,13 +15,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    try {
-      Position position = await Geolocator()
-          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-      print(position);
-    } catch (e) {
-      print(e);
-    }
+    Location location = Location();
+    await location.getCurrentLocation();
   }
 
   @override
